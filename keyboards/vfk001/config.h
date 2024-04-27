86c478999aabd6b8ba57cc250489e51c402b99cf
JH <13332150+oulico@users.noreply.github.com>
2024-04-27 10:51:24 +0900

    FIX: reversed left side keyboard

--- keyboards/vfk001/config.h
+++ keyboards/vfk001/config.h
@@ -1,4 +1,4 @@
-// Copyright 2024 Junghyun Lee (@JH)
+// Copyright 2024 Junghyun Lee (@oulico)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -19,12 +19,18 @@
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 
-#define MATRIX_COL_PINS { D7, D1, D0, D4, C6 }
-#define MATRIX_ROW_PINS { B6, E6, B4, B5  }
-#define DIODE_DIRECTION COL2ROW
-
+/* to use split keyboard */
 #define SPLIT_USB_DETECT 
+
 /* define the handedness of the keyboard. this keyboard will be connected with a cable from left half of the keyboard*/
 #define MASTER_LEFT
+
+#define MATRIX_COL_PINS {C6, D4, D0, D1, D7}
+#define MATRIX_ROW_PINS {B5,B4, E6, B6 }
+
+#define MATRIX_COL_PINS_RIGHT { D7, D1, D0, D4, C6 }
+#define MATRIX_ROW_PINS_RIGHT{ B6, E6, B4, B5  }
+
+#define DIODE_DIRECTION COL2ROW
 
 #define DEBOUNCE 5
