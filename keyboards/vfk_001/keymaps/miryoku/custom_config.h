845df6ac4de96a75e6d06349b45db866d9f95062
JH <13332150+oulico@users.noreply.github.com>
2024-05-05 20:12:51 +0900

    remove unused line from keymap

--- keyboards/vfk_001/keymaps/miryoku/custom_config.h
+++ keyboards/vfk_001/keymaps/miryoku/custom_config.h
@@ -62,7 +62,6 @@ KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_R
 KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
 KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-          U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
