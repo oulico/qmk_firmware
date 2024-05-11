cdbd9172ce91204b0bdac2af7f6960f2abcf2922
JH <13332150+oulico@users.noreply.github.com>
2024-05-11 16:38:51 +0900

    switched command and ctrl

--- keyboards/vfk_001/keymaps/miryoku/custom_config.h
+++ keyboards/vfk_001/keymaps/miryoku/custom_config.h
@@ -65,7 +65,7 @@ U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_U
 
 #define MIRYOKU_LAYER_NAV \
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           CW_TOGG,           \
+KC_LCTL,           KC_LALT,           KC_LGUI,           KC_LSFT,           U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           CW_TOGG,           \
 U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA,              KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            KC_INS,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
 
@@ -76,14 +76,14 @@ KC_GRV,            KC_1,              KC_2,              KC_3,              KC_B
 U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_FUN \
-KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,           U_NA,              U_NA,              U_NA,             U_NA,               U_NA,     \
-KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NA,              KC_LSFT,           KC_LGUI,          KC_LALT,            KC_LCTL,           \
-KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,           U_NA,              U_NA,              U_NA,             U_NA,               U_NA,              \
+KC_F12,            KC_F7,             KC_F8,             KC_F9,             LSFT(LCMD(KC_3)),           U_NA,              U_NA,              U_NA,             U_NA,               U_NA,     \
+KC_F11,            KC_F4,             KC_F5,             KC_F6,             LSFT(LCMD(KC_4)),           U_NA,              KC_LSFT,           KC_LGUI,          KC_LALT,            KC_LCTL,           \
+KC_F10,            KC_F1,             KC_F2,             KC_F3,             LSFT(LCMD(KC_5)),           U_NA,              U_NA,              U_NA,             U_NA,               U_NA,              \
 U_NP,              U_NP,              LCTL(KC_SPC),              KC_SPC,            KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              LSFT(LCMD(KC_Z)),             LCMD(KC_V),             LCMD(KC_C),             LCMD(KC_X),             LCMD(KC_Z),             \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           U_NU,              \
+KC_LCTL,           KC_LALT,           KC_LGUI,           KC_LSFT,           U_NA,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           U_NU,              \
 U_NA,              KC_RALT,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           U_NU,              \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           LCTL(LCMD(KC_D)),           U_NP,              U_NP
 
